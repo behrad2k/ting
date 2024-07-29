61703a53ac842ed3b90e69c1772aa5c6a630d6a7
behrad2k <behradsharifi8@gmail.com>
2024-07-29 18:24:39 +0330

    done universal proghdr strct

--- include/proghdr.h
+++ include/proghdr.h
@@ -3,8 +3,19 @@
 
 typedef struct proghdr {
 	uint32_t type;
-	uint32_t hybrid_32; // flags or 32 offset
-	uint64_t offset_64;
-	uint32_t 
+	uint32_t flags_64; // 64-bit
+	uint64_t offset_64; // 64-bit
+	uint32_t offset; // 32-bit
+	uint64_t vaddr_64; // 64-bit
+	uint32_t vadder; // 32-bit
+	uint64_t paddr_64; // 64-bit
+	uint32_t paddr; // 32-bit
+	uint64_t filesz_64; // 64-bit
+	uint32_t filesz; // 32-bit
+	uint64_t memsz_64; // 64-bit
+	uint32_t memsz; // 32-bit
+	uint32_t flags_32; // 32-bit
+	uint64_t align_64; // 64-bit
+	uint32_t align; // 32-bit
 }
 
